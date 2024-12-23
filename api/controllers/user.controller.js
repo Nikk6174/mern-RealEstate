e6e30b3df07e46be5ceaa11c530aa04987202574
Nikk6174 <nikk.6174@gmail.com>
2024-12-24 00:39:17 +0530

    create a test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+const test = (req, res) => {
+    res.send('Backend is running');
+};
+
+module.exports = { test };
--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const mongoose = require('mongoose'); // Replace import with require
+const userRouter = require('./routes/user.route'); // Import the User model
+
 
 dotenv.config({ path: './api/.env' }); // Load environment variables
 console.log("PORT from .env:", process.env.PORT); 
+
+mongoose.connect(process.env.MONGO).then(() => console.log('Connected to MongoDB')).catch(err => console.log(err)); 
+
+//special url encoding has to be done for @ = %40
+
+
 
 const app = express();
 
@@ -12,9 +21,7 @@ app.use(express.json());
 app.use(cors());
 
 // Test Route
-app.get('/', (req, res) => {
-  res.send('Backend is running');
-});
+app.use('/api/user', userRouter);
 
 // Start the server
 const PORT = process.env.PORT;
